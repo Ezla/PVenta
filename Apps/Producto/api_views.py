1e3c4c3c9add9fabbd83df3a7e8d5b3d0ad0402a
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-06-22 19:49:43 -0500

    Product: Se quita logica para agregar code39 en la api de Producto
    - Ahora se utilisara la logica el signals para que se encarge.
    - Adicionalmente el signal generara un code39 aleatorio, en vez de un
      numero secuencial apartir de la pk, como se hacia hasta ahora.

--- Apps/Producto/api_views.py
+++ Apps/Producto/api_views.py
@@ -33,37 +33,6 @@ class ProductoApiView(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
-    def generate_code39(self):
-        code = str(self.product.pk)
-        while len(code) < 10:
-            code = '0' + code
-        return code
-
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            self.product = serializer.save()
-            if self.product.code39:
-                code39 = self.generate_code39()
-                self.product.codigo = code39
-                self.product.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response( serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        self.product = self.get_object()
-        serializer = self.get_serializer(self.product, data=request.data)
-        if serializer.is_valid():
-            self.product = serializer.save()
-            if self.product.code39:
-                code39 = self.generate_code39()
-                self.product.codigo = code39
-                self.product.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class BrandApiView(viewsets.ModelViewSet):
     queryset = Marca.objects.all()
