2915f5b4b14358a540efa585133a24cd5f0a34e9
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-06-30 00:34:35 -0500

    Statistics: Se muestra cambpos en la tabla de facturas en admin
    - Se agrega un display para el modelo de admin, para fasilitar la
      visualisacion de registros.
    - Se agrega metodo __str__ para el modelo Provider
    - Se identa los modelos de statistics para cumplir los estandares del pep8

--- Apps/Estadistica/admin.py
+++ Apps/Estadistica/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Provider, ForeignInvoice, Payment
 
+
+class ForeignInvoiceAdmin(admin.ModelAdmin):
+    list_display = (
+    'folio', 'provider', 'amount', 'discount', 'discount_applied',
+    'status', 'invoice_date', 'settlement_date')
+
+
 admin.site.register(Provider)
-admin.site.register(ForeignInvoice)
+admin.site.register(ForeignInvoice, ForeignInvoiceAdmin)
 admin.site.register(Payment)
--- Apps/Estadistica/models.py
+++ Apps/Estadistica/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class Provider(models.Model):
-    name = models.CharField(verbose_name='Nombre del proveedor', max_length=45,
+    name = models.CharField(verbose_name='Nombre del proveedor',
+                            max_length=45,
                             unique=True)
-    phone = models.CharField(verbose_name='Número de teléfono', max_length=12)
+    phone = models.CharField(verbose_name='Número de teléfono',
+                             max_length=12)
+
+    def __str__(self):
+        return self.name
 
 
 class ForeignInvoice(models.Model):
@@ -18,14 +23,17 @@ class ForeignInvoice(models.Model):
         (CANCELLED, 'Cancelada')
     )
 
-    folio = models.CharField(verbose_name='Folio de la factura', max_length=12)
-    amount = models.DecimalField(verbose_name='Monto a pagar', max_digits=8,
+    folio = models.CharField(verbose_name='Folio de la factura',
+                             max_length=12)
+    amount = models.DecimalField(verbose_name='Monto a pagar',
+                                 max_digits=8,
                                  decimal_places=2)
     provider = models.ForeignKey(to=Provider, on_delete=models.CASCADE)
     discount = models.PositiveIntegerField(
         verbose_name='Porcentaje de descuento')
-    discount_applied = models.BooleanField(verbose_name='Descuento aplicado',
-                                           default=False)
+    discount_applied = models.BooleanField(
+        verbose_name='Descuento aplicado',
+        default=False)
     status = models.CharField(verbose_name='Estado de la factura',
                               choices=INVOICE_STATUS_CHOICES, max_length=1)
     invoice_date = models.DateField(verbose_name='Fecha de la factura')
@@ -45,7 +53,8 @@ class Payment(models.Model):
         (CHECK, 'Cheque ')
     )
 
-    invoice = models.ForeignKey(to=ForeignInvoice, on_delete=models.CASCADE)
+    invoice = models.ForeignKey(to=ForeignInvoice,
+                                on_delete=models.CASCADE)
     reference = models.CharField(verbose_name='Referencia del pago',
                                  max_length=20)
     amount = models.DecimalField(verbose_name='Monto pagado', max_digits=8,
