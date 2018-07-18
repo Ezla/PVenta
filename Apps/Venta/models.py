3cd8836a86b813a65b2ef6adfa4e62843efce742
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-07-18 18:44:26 -0500

    Sale: Se cambia modelos para venta
    - Se cambian los modelos para tener mas congruencia con un standar de
      programacion.
    - Se eliminan campos redundandantes o no necesarios.

--- Apps/Venta/models.py
+++ Apps/Venta/models.py
@@ -1,47 +1,56 @@
-from django.db import models
+from decimal import Decimal
 from django.core.exceptions import ValidationError
+from django.db import models
 
 
-def default_descuento():
-    descuento = Descuento.objects.get_or_create(descuento=0)
-    return descuento[0].pk
-
-
-class Descuento(models.Model):
-    descuento = models.PositiveSmallIntegerField(unique=True)
+class Discount(models.Model):
+    percentage = models.PositiveSmallIntegerField(unique=True)
 
     def __str__(self):
-        return 'Descuento del {}%'.format(self.descuento)
+        return 'Descuento del {}%'.format(self.percentage)
 
     def clean(self):
-        if self.descuento > 100:
+        if self.percentage > 100:
             msg = 'Este campo no puede contener numeros mayores a 100.'
             raise ValidationError({'descuento': msg})
 
 
-class Cuenta(models.Model):
-    tiket = models.CharField(max_length=30, null=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    efectivo = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    cambio = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    creado = models.DateTimeField(auto_now_add=True, null=True)
-    modificado = models.DateTimeField(auto_now=True, null=True)
-    descuento = models.ForeignKey(Descuento, on_delete=models.CASCADE,
-                                  default=default_descuento)
+class SalesAccount(models.Model):
+    ticket = models.CharField(max_length=30, blank=True)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    cash = models.DecimalField(max_digits=10, decimal_places=2)
+    change_due = models.DecimalField(max_digits=8, decimal_places=2)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.tiket
+        return self.ticket
+
+    @property
+    def discount_amount(self):
+        return (self.subtotal * Decimal(
+            self.discount.percentage)) / Decimal(100)
+
+    @property
+    def total(self):
+        return self.subtotal - self.discount_amount
 
 
-class Venta(models.Model):
-    codigo = models.CharField(max_length=48, null=True)
-    nombre = models.CharField(max_length=100, null=True)
-    descuento = models.BooleanField(default=False)
-    precio = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    cantidad = models.IntegerField(null=True)
-    subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    cuenta = models.ForeignKey(Cuenta, on_delete=models.SET_NULL, null=True)
+class SalesProduct(models.Model):
+    code = models.CharField(max_length=48)
+    name = models.CharField(max_length=100)
+    with_discount = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
+    sales_account = models.ForeignKey(SalesAccount,
+                                      on_delete=models.CASCADE,
+                                      related_name='products',
+                                      blank=True, null=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
+
+    @property
+    def total(self):
+        return self.price * self.quantity
