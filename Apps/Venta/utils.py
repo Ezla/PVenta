3e2d193e157774b60e28b84b5c3a97046b334b17
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-07-18 20:18:16 -0500

    Sale: Se agrega utilidad para calcular precios en el carrito
    - Esta funcion regresara el total, subtotal y el descuento de los
      productos que esten en el carrito de compra (se envian como parametro).

---
+++ Apps/Venta/utils.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+
+
+def process_cart(cart=list(), percent_off=0):
+    """
+    Calcula el precio total, subtotal y descuento de los productos en el
+    carrito de compras.
+    :param cart: (list) lista de productos que estan el el carrito.
+    Esta lista lleva un formato del modelo SalesProduct.
+    :param percent_off: (int) porcentaje de descuento aplicable.
+    :return: (Decimal) cantidades calculadas de subtotal, total y
+    descuento a partir de los productos que hay en el carrito.
+    """
+    subtotal = Decimal(0)
+
+    for product in cart:
+        with_discount = product.get('with_discount')
+        quantity = product.get('quantity')
+        if not with_discount:
+            price = product.get('price_up')
+        else:
+            price = product.get('price_down')
+
+        subtotal_product = Decimal(price) * Decimal(quantity)
+        subtotal += subtotal_product
+
+    discount = (subtotal * Decimal(percent_off)) / Decimal(100)
+    total = subtotal - discount
+    return subtotal, total, discount
