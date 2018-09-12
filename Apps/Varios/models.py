513bd37f2db65a2f843d788790b7d82157d45fb7
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-09-12 18:08:21 -0500

    Varios: Se agrega nuevo modelo para las listas
    - Con este nuevo modelo se pretende generar listas para productos como
      monografias,mapas y biografias, que permitira tener un control.

--- Apps/Varios/models.py
+++ Apps/Varios/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Listed(models.Model):
+    MONOGRAPH = '1'
+    BIOGRAPHY = '2'
+    MAP = '3'
+    TYPE_CHOICES = (
+        (MONOGRAPH, 'Monografía'),
+        (BIOGRAPHY, 'Biografia'),
+        (MAP, 'Mapa')
+    )
+    name = models.CharField(verbose_name='Nombre', max_length=100,
+                            unique=True)
+    number = models.IntegerField(verbose_name='Numero de lista', default=0)
+    type = models.CharField(verbose_name='Tipo de producto',
+                            choices=TYPE_CHOICES, max_length=1)
+    active = models.BooleanField(verbose_name='Activar para listar',
+                                 default=True)
+
+    def __str__(self):
+        return self.name
