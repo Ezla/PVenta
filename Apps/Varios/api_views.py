84cfbdf9ad04e7d05531eb258b3a630c1b7b4dd1
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-10-14 13:24:48 -0500

    Various: Ordenamiento inicial por nombre en los listados
    - Se agrega link en el menu para acceer a los listados
    - Ordenamiento inicial en funcion al nombre.

--- Apps/Varios/api_views.py
+++ Apps/Varios/api_views.py
@@ -8,7 +8,21 @@ from .models import Listed
 class ApiListedView(APIView):
 
     def get(self, request):
-        query = Listed.objects.all()
+        query = Listed.objects.all().order_by('name')
         listed = ListedSerialiser(query, many=True)
 
+        return Response(listed.data, status=status.HTTP_200_OK)
+
+
+class ApiEnumerateListingView(APIView):
+
+    def get(self, request):
+        query = Listed.objects.all().order_by('name')
+        print('entro')
+        count = 0
+        for item in query:
+            count += 1
+            item.number = count
+            item.save()
+        listed = ListedSerialiser(query, many=True)
         return Response(listed.data, status=status.HTTP_200_OK)
