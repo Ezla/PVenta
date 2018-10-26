c56f192903b4a4a3619d80ecbaef778dba5c65d4
Abraham Rodarte <bg_kuriboh@hotmail.com>
2018-10-26 17:41:04 -0500

    Various: se agrega soporte para dar de alta registro
    - la apiu ahora soporta el agregar nuevos registros
    - Se agrega modal con formulario para gestionar los registros.
    - Se agrega soporte para que el usuario pueda agregar nuevos registros a
      la tabla

--- Apps/Varios/api_views.py
+++ Apps/Varios/api_views.py
@@ -13,6 +13,15 @@ class ApiListedView(APIView):
 
         return Response(listed.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        serializer = ListedSerialiser(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+
 
 class ApiEnumerateListingView(APIView):
 
--- static/js/various/listed.js
+++ static/js/various/listed.js
@@ -1,6 +1,31 @@
 var datatable = undefined;
 
 /**
+ * Regresa un registro jquey (tr) listo para ser agregado a la tabla.
+ * @param {object} item: datos del registro para agregar en al tabla.
+ * @returns {jQuery}
+ */
+function renderRecord(item) {
+    var name;
+    if (item.type == '1') {
+        name = 'Monografía';
+    } else if (item.type == '2') {
+        name = 'Biografia';
+    } else if (item.type == '3') {
+        name = 'Mapa';
+    }
+    var $number = $('<td/>', {'text': item.number});
+    var $name = $('<td/>', {'text': item.name});
+    var $type = $('<td/>', {'text': name});
+    var $active = $('<td/>', {'text': item.active});
+    var $tr = $('<tr/>', {
+        'role': 'button',
+        'data-code': item.pk
+    }).append($number, $name, $type, $active);
+    return $tr
+}
+
+/**
  * Dibuja los registros en la tabla (tr) y aplica el cargado de DataTable
  * en la tabla generada.
  * @param {array} products: lista de productos.
@@ -9,22 +34,7 @@ function renderListed(products) {
     $('#table-body').html('');
     if (products.length > 0) {
         $.each(products, function (i, item) {
-            var name;
-            if (item.type == '1') {
-                name = 'Monografía';
-            } else if (item.type == '2') {
-                name = 'Biografia';
-            } else if (item.type == '3') {
-                name = 'Mapa';
-            }
-            var $number = $('<td/>', {'text': item.number});
-            var $name = $('<td/>', {'text': item.name});
-            var $type = $('<td/>', {'text': name});
-            var $active = $('<td/>', {'text': item.active});
-            var $tr = $('<tr/>', {
-                'role': 'button',
-                'data-code': item.pk
-            }).append($number, $name, $type, $active);
+            var $tr = renderRecord(item);
             $('#table-body').append($tr);
         });
         datatable = $('#table-listed').DataTable({
@@ -33,6 +43,12 @@ function renderListed(products) {
                 "url": "/static/json/tools/datatables/Spanish.json"
             },
             buttons: [
+                {
+                    text: 'Re numerar',
+                    action: function (e, dt, node, config) {
+                        $("#confirmation-modal").modal('show');
+                    }
+                },
                 'csv',
                 {
                     extend: 'pdfHtml5',
@@ -42,9 +58,17 @@ function renderListed(products) {
                     },
                 },
                 {
-                    text: 'Re numerar',
+                    text: 'Agregar nuevo',
                     action: function (e, dt, node, config) {
-                        $("#confirmation-modal").modal('show');
+                        $('#id_name').val('');
+                        $('#id_number').val(0);
+                        $('#id_type').val('');
+                        $('#id_active').prop('checked', true);
+                        $('#help-name').text('').parent().removeClass('has-error');
+                        $('#help-number').text('').parent().removeClass('has-error');
+                        $('#help-type').text('').parent().removeClass('has-error');
+                        $('#help-active').text('').parent().removeClass('has-error');
+                        $("#listed-modal").modal('show');
                     }
                 }
             ],
@@ -97,6 +121,19 @@ function reNumberListed() {
 }
 
 /**
+ * Gestiona la consulta al servidor para guardar el registro.
+ */
+function saveListed() {
+    var data = {
+        'name': $('#id_name').val(),
+        'number': $('#id_number').val(),
+        'type': $('#id_type').val(),
+        'active': $('#id_active').prop('checked')
+    };
+    runAjax(data, '/api/listed/', 'post');
+}
+
+/**
  * Realiza la petición al servidor mediante ajax.
  * @param {object} data: Datos que se enviaran en la petición.
  * @param {string} url: Dirección a la cual se realizara la petición.
@@ -122,8 +159,26 @@ function runAjax(data, url, methodType) {
                 $('#table-body').show();
                 datatable.destroy();
                 renderListed(data);
+            } else if (methodType == 'post') {
+                $("#listed-modal").modal('hide');
+                var $newTr = renderRecord(data);
+                datatable.row.add($newTr).draw();
             }
         },
+        error: function (jqXHR) {
+            var errors = jqXHR.responseJSON;
+            $.each(errors, function(key, msg){
+                if (key == 'name') {
+                    $('#help-name').text(msg).parent().addClass('has-error');
+                } else if (key == 'number') {
+                    $('#help-number').text(msg).parent().addClass('has-error');
+                } else if (key == 'type') {
+                    $('#help-type').text(msg).parent().addClass('has-error');
+                } else if (key == 'active') {
+                    $('#help-active').text(msg).parent().addClass('has-error');
+                }
+            });
+        }
     });
 }
 
@@ -131,3 +186,5 @@ $(document).ready(function () {
     getListed();
     initalModal();
 });
+
+$('#guardar').on('click', saveListed);
